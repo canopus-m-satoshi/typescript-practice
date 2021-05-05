645b534b60575c4f4f408f511bce62d136b797bb
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-05 15:42:14 +0900

    49. React × TypeScriptの準備 https://www.udemy.com/course/react_stepup/learn/lecture/24823582#content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import axios from "axios";
 import "./styles.css";
 
 export default function App() {
+  const onCLickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onCLickFetchData}>データ取得</button>
     </div>
   );
 }
