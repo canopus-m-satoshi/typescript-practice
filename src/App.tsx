d8edd3f911183293f831af9dc1c7a3c7e5ac1ec8
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-05 16:16:53 +0900

    50. 型がないせいでバグっているアプリの例 https://www.udemy.com/course/react_stepup/learn/lecture/24823584#content

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import axios from "axios";
+
+import { useState } from "react";
+
 import "./styles.css";
 
+import { Todo } from "./Todo";
+
 export default function App() {
-  const onCLickFetchData = () => {
+  const [todos, setTodos] = useState<any>([]);
+
+  const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTodos(res.data);
     });
   };
 
   return (
     <div className="App">
-      <button onClick={onCLickFetchData}>データ取得</button>
+      <button onClick={onClickFetchData}>データ取得</button>
+
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
