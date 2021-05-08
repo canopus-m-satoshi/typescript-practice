71f540695db2921e13b0468bc6b583f0c9ae439b
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-08 23:39:24 +0900

    53. 型定義を効率的に管理しよう https://www.udemy.com/course/react_stepup/learn/lecture/24823596#overview

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,9 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean; // 無くてもエラーにならない
-};
+import { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+// Omit : 省略するpropsを指定
+// Pick : 使用したいpropsを指定
+
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
