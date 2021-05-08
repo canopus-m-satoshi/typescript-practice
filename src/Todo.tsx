eb7cff618148b04e57e635f8220e89dfb325b7ab
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-08 23:51:51 +0900

    54. コンポーネント自体の型定義 https://www.udemy.com/course/react_stepup/learn/lecture/24823600#overview

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,9 +1,10 @@
+import { VFC } from "react";
 import { TodoType } from "./types/todo";
 
 // Omit : 省略するpropsを指定
 // Pick : 使用したいpropsを指定
 
-export const Todo = (props: Omit<TodoType, "id">) => {
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
